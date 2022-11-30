e2b78c8ce26d2ad765f5f9e29a7745c8d3f4b08b
Jakub Wilk <jwilk@jwilk.net>
2022-11-30 18:09:07 +0100

    vim/highlight.py: add decorator highlight bug.

--- vim/highlight.py
+++ vim/highlight.py
@@ -1,3 +1,11 @@
 0 == 0_0
 
 'foo' == r'foo' == fr'foo' == rf'foo'
+
+# ------------------------------------------------------------------------
+
+if True:
+    foo
+    @decorator  # ← not highlighted as decorator
+    def bar():
+        pass
