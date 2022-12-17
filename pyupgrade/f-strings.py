51cb9ebb965a5e4ef8fd9abf6b0a24b8dffba1e7
Jakub Wilk <jwilk@jwilk.net>
2022-12-17 10:53:51 +0100

    pyupgrade/f-strings: reword comment.

--- pyupgrade/f-strings.py
+++ pyupgrade/f-strings.py
@@ -1,4 +1,4 @@
-s = '{foo}{bar}'.format(foo=foo, bar=bar)  # this is rewritten as f-strings
+s = '{foo}{bar}'.format(foo=foo, bar=bar)  # this is pyupgraded to f-string
 s = '{foo}{bar}'.format(  # but this is not
     foo=foo,
     bar=bar,
